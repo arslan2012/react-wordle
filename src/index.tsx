37feeebd33daaf40a0a7f9f49c0934b1b40558b8
Arslan Ablikim <arslan.ablikim0@gmail.com>
2024-05-09 10:24:12 +0800

    added typescript

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,11 @@ import { createRoot } from "react-dom/client";
 
 import App from "./App";
 
-const rootElement = document.getElementById("root");
+const rootElement = document.getElementById("root")!;
 const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <App />
+    <App/>
   </StrictMode>
 );
