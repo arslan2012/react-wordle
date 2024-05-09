cf0102eb4a411f814b3924252fbd0ab716a70d84
Arslan Ablikim <arslan.ablikim0@gmail.com>
2024-05-09 09:01:43 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>React Wordle</h1>
+      <p>CyberConnect Frontend Coding Interview</p>
+    </div>
+  );
+}
